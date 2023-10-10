000d302c3b60007b1ed796e429ae43f98f9e27fa
tmen427 <menx0003@umn.edu>
2023-10-10 12:34:51 -0500

    'updates'

--- vector.js
+++ vector.js
@@ -8,31 +8,35 @@ import { RetrievalQAChain } from "langchain/chains";
 import { ChatOpenAI } from "langchain/chat_models/openai";
 import { OpenAI } from "langchain/llms/openai";
 import 'dotenv/config';
+
+
 import express from "express";
 const app = express()
 import cors from "cors"; 
 
 
-
-const ChatGP = async(search)=> {
-//loading the document 
-const loader = new CSVLoader("./toptwenty.csv");
+const StoredVector= async(search) => {
+//load the document 
+const loader = new CSVLoader("./speech.txt");
+//const loader = new CSVLoader("./half.csv");
 const docs = await loader.load();
 
-console.log("this is your api key")
-console.log(process.env.OPENAI_API_KEY1)
-console.log(process.env.COHERE_API_KEY)
 
 let llm = new OpenAI({
-     openAIApiKey: process.env.OPENAI_API_KEY1,
-   });
+  openAIApiKey: process.env.OPENAI_API_KEY1,
+});
 
-//get a response from chatgpt
- // llm = new ChatOpenAI({ modelName: "gpt-3.5-turbo" });
+ //llm = new ChatOpenAI({ modelName: "gpt-3.5-turbo" });  
 
-const embeddings = new CohereEmbeddings({
-  apiKey: process.env.COHERE_API_KEY, // In Node.js defaults to process.env.COHERE_API_KEY
-  batchSize: 48, // Default value if omitted is 48. Max value is 96
+// const embeddings = new CohereEmbeddings({
+//   apiKey: process.env.COHERE_API_KEY, // In Node.js defaults to process.env.COHERE_API_KEY
+//   batchSize: 96, // Default value if omitted is 48. Max value is 96
+// });
+
+//the batchSize is immportant, if the batchsize is smaller then size of the document the you will get errors
+const embeddings = new OpenAIEmbeddings({
+  openAIApiKey: process.env.OPENAI_API_KEY1, // In Node.js defaults to process.env.OPENAI_API_KEY
+  batchSize: 512, // Default value if omitted is 512. Max is 2048
 });
 
 //stored as a vector file
@@ -41,23 +45,22 @@ const embeddings = new CohereEmbeddings({
    embeddings
  );
 
- const chain = RetrievalQAChain.fromLLM(llm, vectorStore.asRetriever());
-  
-   const response = await chain.call({
-    query: search
-  });
+  const chain = await RetrievalQAChain.fromLLM(llm, vectorStore.asRetriever());
+  const response = await chain.call({
+  query: search
+});
+    return response; 
+ }
 
-  return response; 
-}
+
+
 
 
 
   app.use(cors())
 
   app.get('/', function (req, res) {
-   // res.send('Hello World')
-    res.send(process.env.openapi_key)
-
+  res.send("hello world")
   })
   
   
@@ -65,7 +68,7 @@ const embeddings = new CohereEmbeddings({
     //res.send(req.params.id)
     try {
       console.log(req.params.id)
-    let response = await ChatGP(req.params.id); 
+    let response = await StoredVector(req.params.id); 
     res.send(response.text);
     }
     catch(error) {
